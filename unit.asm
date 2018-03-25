0582243208645b3ebbcc5cf7359fb918ebbe65b9
SAURABH KUMAR <33839674+srvkmr130@users.noreply.github.com>
2018-03-25 13:15:52 +0530

    data section created.

---
+++ unit.asm
@@ -0,0 +1,49 @@
+	.file	"unit.c"
+	.section	.rodata
+.LC0:
+	.string	"Enter the choice"
+.LC1:
+	.string	"1.Length"
+.LC2:
+	.string	"2.Temperature"
+.LC3:
+	.string	"%d"
+	.align 8
+.LC4:
+	.string	"Enter the value to be converted"
+.LC5:
+	.string	"%f"
+.LC6:
+	.string	"Enter the conversion type"
+.LC7:
+	.string	"1.Centimeter"
+.LC8:
+	.string	"2.Inch"
+.LC9:
+	.string	"3.Foot"
+.LC10:
+	.string	"4.Meter"
+.LC11:
+	.string	"5.KM"
+.LC13:
+	.string	"The centimeter value is:%f cm"
+.LC15:
+	.string	"The inch value is:%f in"
+.LC17:
+	.string	"The foot value is:%f ft"
+.LC18:
+	.string	"The meter value is:%f cm"
+.LC20:
+	.string	"The kiloimeter value is:%f cm"
+.LC21:
+	.string	"1.Cesius"
+.LC22:
+	.string	"2.Fahrenheit"
+.LC23:
+	.string	"3.Kelvin"
+.LC24:
+	.string	"The Celsius value is:%f cm"
+.LC26:
+	.string	"The Fahrenheit value is:%f in"
+.LC28:
+	.string	"The Kelvin value is:%f ft"
